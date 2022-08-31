f301d64ad5791b665e1d1b0c32d3a130e5d6e2c8
Gerrit Lane <lane.gerrit@gmail.com>
2022-08-30 20:34:08 -0700

    Serve html and css files from express app

--- js/index.js
+++ js/index.js
@@ -1,8 +1,16 @@
 const express = require('express');
 const app = express();
+const path = require('path');
 app.listen(3000, function(){
 	console.log('Server started on port 3000');
 });
-// app.get('/', (req, res) => {
-//   res.sendFile(__dirname)
-// });
+
+app.get('/', (req, res) => {
+	// res.send(__dirname + '/../')
+  res.sendFile(path.join(__dirname, '../index.html'));
+  // res.sendFile(path.join(__dirname, '../css/styles.css'));
+});
+
+app.get('/css/styles.css', (req, res) => {
+  res.sendFile(path.join(__dirname, '../css/styles.css'));
+});
