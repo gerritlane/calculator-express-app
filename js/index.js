2d5eeb0d74240936184a096400240f4fddde8bb4
Gerrit Lane <lane.gerrit@gmail.com>
2022-08-31 19:53:36 -0700

    Add post method and parser to calculate

--- js/index.js
+++ js/index.js
@@ -1,16 +1,26 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+
+app.use(express.urlencoded({ extended: true }));
+
 app.listen(3000, function(){
 	console.log('Server started on port 3000');
 });
 
 app.get('/', (req, res) => {
-	// res.send(__dirname + '/../')
   res.sendFile(path.join(__dirname, '../index.html'));
-  // res.sendFile(path.join(__dirname, '../css/styles.css'));
 });
 
 app.get('/css/styles.css', (req, res) => {
   res.sendFile(path.join(__dirname, '../css/styles.css'));
+});
+
+// TODO: Double check for XSS vulnerability
+app.post('/index.html', (req, res) => {
+  console.log(req.body);
+  let num1 = Number(req.body.num1);
+  let num2 = Number(req.body.num2);
+  let result = num1 + num2;
+  res.send(`The result of the calculation is ${result}`);
 });
